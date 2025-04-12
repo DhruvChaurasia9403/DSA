37b91a2b24da37a229e88106260088ed090fce13
Dhruv Chaurasia <www.thedc2@gmail.com>
2025-04-12 14:42:37 +0530

    Trees practice

--- Tree/checkBalancedTree.java
+++ Tree/checkBalancedTree.java
@@ -1,26 +1,30 @@
 package Tree;
 
-import java.util.List;
-
-public class MaxHeight {
+public class checkBalancedTree {
     static class Node{
         int val ;
-        Node left;
-        Node right;
+        Node left , right;
         Node(int val){
             this.val = val;
             left=right=null;
         }
     }
+    public static boolean isBalanced(Node node){
+        int height = maxH(node);
+        return height!=-1;
+    }
     public static int maxH(Node node){
-        if(node==null) return 0;
+        if(node == null) return 0;
         int left = maxH(node.left);
         int right = maxH(node.right);
+        if(left == -1 || right==-1) return -1;
+        if(Math.abs(left-right) > 1){
+            return -1;
+        }
         return 1+Math.max(left,right);
     }
-
     public static void main(String[] args) {
-        Node node = new Node(1);
+       Node node = new Node(1);
         node.left = new Node(2);
         node.right = new Node(3);
         node.left.left = new Node(4);
@@ -30,7 +34,9 @@ public class MaxHeight {
         node.left.right.left = new Node(8);
         node.right.right.left = new Node(9);
         node.right.right.right = new Node(10);
+        node.right.right.right.right = new Node(11);
 
-        System.out.println(maxH(node));
+
+        System.out.println("Tree is balanced ? : " +isBalanced(node));
     }
 }
