1146aec3d0a7848cc0fa81d154912ad66ceefbdf
Dhruv Chaurasia <www.thedc2@gmail.com>
2025-09-20 12:46:53 +0530

    Sliding window maximum

--- slidingWindowSubArrayLessThanK.java
+++ slidingWindowSubArrayLessThanK.java
@@ -1,12 +1,34 @@
 import java.util.*;
 public class slidingWindowSubArrayLessThanK{
     public static void main(String[] args) {
-//        int n = 12345;
-//        String s = Integer.toString(n);
-//        System.out.println(s.length());
-        String a = "a";
-        String b = "b";
-        System.out.println(a+b);
-        System.out.println(2%5);
+        int[] arr = {1,3,-1,-3,5,3,6,7};
+        int k = 3;
+        int[] nums = swm(arr,k);
+        for(int i : nums){
+            System.out.println(i);
+        }
+    }
+    public static int[] swm(int[] arr, int k){
+        int[] nums = new int[arr.length-k+1];
+        int x = 0;
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i = 0 ; i < k ; i++){
+            while(!dq.isEmpty() && arr[dq.peekLast()]<=arr[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        nums[x++] = arr[dq.peekFirst()];
+        for(int i = k ; i < arr.length ; i++){
+            while(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && arr[dq.peekLast()]<arr[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            nums[x++] = arr[dq.peekFirst()];
+        }
+        return nums;
     }
 }
